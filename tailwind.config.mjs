ddc3ec92820e4575428636cf81f6afdd51e60a46
mulandi <mulandimorris1@gmail.com>
2025-02-28 20:29:44 +0300

    navbar done 1.1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+ content: [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+ ],
+ theme: {
+  extend: {
+   colors: {
+    lighHover: '#fcf4ff',
+    darkHover: '#2a004a',
+    darkTheme: '#11001F',
+   },
+   fontFamily: {
+    Outfit: ["Outfit", "sans-serif"],
+    Ovo: ["Ovo", "serif"]
+   }
   },
-  plugins: [],
+ },
+ plugins: [],
 };
