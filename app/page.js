f52ff87518b3c72ceb7170e806f0ea485cb9d29e
mulandi <mulandimorris1@gmail.com>
2025-03-01 08:31:31 +0300

    header done 1.1

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 "use client"
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Head from 'next/head';
 
 export default function Home() {
  return (
   <>
    <Navbar />
+   <Header/>
   </>
  );
 }
