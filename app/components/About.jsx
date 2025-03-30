4cfeba93c17cfb7e2b387e7a4aabaedd848e88d2
mulandi <mulandimorris1@gmail.com>
2025-03-30 23:18:56 +0300

    reformating

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,39 +10,40 @@ const About = ({ isDarkMode }) => {
    initial={{ opacity: 0 }}
    whileInView={{ opacity: 1 }}
    transition={{ duration: 1 }}
-
-
    id='about' className='w-full px-[12%] py-10 scroll-mt-20' >
+
    <motion.h4
     initial={{ opacity: 0, y: -20 }}
     whileInView={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5, delay: 0.3 }}
-
     className='text-center mb-2 text-lg font-Ovo' >
+
     Introduction
+
    </motion.h4>
    <motion.h2
     initial={{ y: -20, opacity: 0 }}
     whileInView={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.5, delay: 0.5 }}
-
     className='text-center text-5xl font-Ovo' >
-    About me
-   </motion.h2>
 
+    About me
+
+   </motion.h2>
    <motion.div
     initial={{ opacity: 0 }}
     whileInView={{ opacity: 1 }}
     transition={{ duration: 0.8 }}
-
     className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+
     <motion.div
      initial={{ scale: 0.9, opacity: 0 }}
      whileInView={{ scale: 1, opacity: 1 }}
      transition={{ duration: 0.6 }}
-
      className='w-64 sm:w-80 rounded-3xl max-w-none' >
+
      <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
+
     </motion.div>
 
     <motion.div
@@ -59,14 +60,12 @@ const About = ({ isDarkMode }) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.8, delay: 1 }}
-
       className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl' >
+
       {infoList.map(({ icon, iconDark, title, description, link }, index) => (
        <motion.li href={link}
         whileHover={{ scale: 1.05 }}
-
         key={index} className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lighHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50 '>
-        
         <Image src={isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3' />
         <h3 className='my-4 font-semibold text-gray-700 dark:text-white' >{title}</h3>
         <p className='text-gray-600 text-sm dark:text-white/80' >{description}</p>
@@ -79,14 +78,17 @@ const About = ({ isDarkMode }) => {
       initial={{ y: 20, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5, delay: 1.3 }}
+      className='my-6 text-gray-700 font-Ovo dark:text-white/80' >
+      
+      Tools I use
 
-      className='my-6 text-gray-700 font-Ovo dark:text-white/80' >Tools I use</motion.h4>
+     </motion.h4>
      <motion.ul
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.6, delay: 1.5 }}
-
       className='flex items-center gap-3 sm:gap-5'>
+      
       {toolsData.map((tool, index) => (
        <motion.li
         whileHover={{ scale: 1.1 }}
